4f563a45e22333d533914ec3865b150dc6cfbe13
Mohamed Aziz Nabli <aziznabli7790@gmail.com>
2022-12-16 22:32:19 +0100

    ping pong test

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+require('dotenv/config') 
+const client = new Client({ 
+  intents: [
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.MessageContent,
+  
+  ], 
+});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
+client.on('messageCreate', async message => {
+  if (message.content == "!ping"){
+    message.reply('pong');
   }
 });
 
