bacf261d29cf83e3beeddccf507731ecf2d9cb22
Mohamed Aziz Nabli <aziznabli7790@gmail.com>
2022-12-19 11:06:10 +0100

    added status

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -2,8 +2,9 @@ const memberC = require("../../Controllers/Member");
 module.exports =  {
     name:"ready",
     once: true,
-    execute(){
+    execute(a,client){
         console.log('the client is now ready');
+        client.user.setActivity("Game"); 
     }
 
 }
